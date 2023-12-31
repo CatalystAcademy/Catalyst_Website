1bbad6becade0e5034b59bf5db45bc1d3df3db8d
CatalystAcademy <info@catalystsacademy.com>
2023-12-31 18:35:12 +0400

    added design file, but some issues with jquery plugins still exist

---
+++ catalyst_academy.client/newFile.js
@@ -0,0 +1,6 @@
+﻿import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import plugin from '@vitejs/plugin-vue';
+import fs from 'fs';
+import { keyFilePath, certFilePath } from './vite.config';
+
---
+++ catalyst_academy.client/src/main.js
@@ -0,0 +1,33 @@
+// Import jQuery and jQuery plugins
+import $ from 'jquery'
+import './assets/js/jquery.min.js'
+import './assets/js/jquery.validate.min.js'
+import './assets/js/bootstrap.bundle.min.js'
+import './assets/js/countdown.min.js'
+import './assets/js/jquery.magnific-popup.min.js'
+// import './assets/js/jquery.counterup.min.js'
+import './assets/js/owl.carousel.min.js'
+import './assets/js/TweenMax.min.js'
+/*import './assets/js/vegas.min.js'
+import './assets/js/waypoints.min.js'
+import './assets/js/wow.js'*/
+
+
+
+import './assets/css/animate.min.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/magnific-popup.css'
+import './assets/css/owl.carousel.min.css'
+import './assets/css/owl.theme.default.min.css'
+import './assets/css/responsive.css'
+import './assets/css/style.css'
+import './assets/plugins/kipso-icons/style.css'
+import './assets/css/vegas.min.css'
+import './assets/plugins/fontawesome/css/all.min.css'
+
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
